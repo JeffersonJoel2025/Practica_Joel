550f4ac8903565a3a38311c724ef5d127752cb6d
Joel Gonzalez <joel202590@outlook.com>
2025-03-05 19:28:47 -0600

    Agregar archivos de proyecto.

---
+++ PracticaJoel.AppMVCDataBaseFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PracticaJoel.AppMVCDataBaseFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
