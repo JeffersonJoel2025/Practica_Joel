2e149a0b4137338b96f7f9f194897c27976eb305
Joel Gonzalez <joel202590@outlook.com>
2025-03-08 17:50:59 -0600

    Se descargaron los paquetes nuget y se crearon las clases de Empleado y Alumno para el proyecto codefirst

---
+++ PracticaJoel.AppMVCCodeFirst/Models/Alumno.cs
@@ -0,0 +1,20 @@
+﻿namespace PracticaJoel.AppMVCCodeFirst.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Codigo { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime FechaNacimineto { get; set; }
+
+        public short YearEstudio { get; set; }
+
+    }
+}
