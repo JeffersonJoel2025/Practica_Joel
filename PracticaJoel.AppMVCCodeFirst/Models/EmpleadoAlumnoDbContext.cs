1495918396e312191c795a1e587308f18d76f794
Joel Gonzalez <joel202590@outlook.com>
2025-03-08 18:01:32 -0600

    Se creo la clase EmpleadoAlumnoDbContext y se agrego la cadena de conexion en el appsentings

---
+++ PracticaJoel.AppMVCCodeFirst/Models/EmpleadoAlumnoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticaJoel.AppMVCCodeFirst.Models
+{
+    public class EmpleadoAlumnoDbContext : DbContext
+    {
+        public EmpleadoAlumnoDbContext(DbContextOptions<EmpleadoAlumnoDbContext> options) : base(options) { }
+
+        public DbSet<Empleado> empleados { get; set; }
+
+        public DbSet<Alumno> alumnoes { get; set; }
+    }
+}
